0e73d10cbe53d7a5ed0e43dcaaec71fc5459ca37
Firas Khalil Khana <firasuke@gmail.com>
2021-01-01 14:04:24 +0200

    New Year 2021

--- dash-colors.sh
+++ dash-colors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/dash -e
 
-# Copyright (c) 2020, Firas Khalil Khana
+# Copyright (c) 2020-2021, Firas Khalil Khana
 # Distributed under the terms of the ISC License
 
 for i in $(seq 30 37) $(seq 40 48); do
